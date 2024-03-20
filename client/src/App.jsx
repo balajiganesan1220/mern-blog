31b8dc7938042c8914bd9819e615e191bee0e699
balajiganesan1220 <balajiganesan1220@gmail.com>
2024-03-20 13:55:40 +0530

    Create footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Projects from './pages/Projects'
 import Header from './components/Header'
+import FooterCom from './components/Footer';
 
 function App() {
   return (
@@ -19,7 +20,8 @@ function App() {
         <Route path="/sign-up" element={<SignUp/>} />
         <Route path="/dashboard" element={<Dashbord />} />
         <Route path='/projects' element={<Projects/>}/>
-   </Routes>
+      </Routes>
+      <FooterCom />
     </BrowserRouter>
   )
 }
